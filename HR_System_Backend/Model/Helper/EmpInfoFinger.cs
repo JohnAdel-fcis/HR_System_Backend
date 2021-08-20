e80605ab7f5f6c1d680208e9a6508c5572d437f1
john <johnadel2018@gmail.com>
2021-08-20 17:48:26 +0200

    Add Name To response

--- HR_System_Backend/Model/Helper/EmpInfoFinger.cs
+++ HR_System_Backend/Model/Helper/EmpInfoFinger.cs
@@ -5,8 +5,9 @@ namespace HR_System_Backend.Model.Helper
     public class EmpInfoFinger
     {
         public int idwEnrollNumber { get; set; }
-        public int idwVerifyMode { get; set; }
-        public int idwInOutMode { get; set; }
+        public string name { get; set; }
+        public int? idwVerifyMode { get; set; }
+        public int? idwInOutMode { get; set; }
         public DateTime LogDate { get; set; }
         public TimeSpan LogTime { get; set; }
     }
--- HR_System_Backend/Repository/Repository/FingerRepository.cs
+++ HR_System_Backend/Repository/Repository/FingerRepository.cs
@@ -212,16 +212,22 @@ namespace HR_System_Backend.Repository.Repository
 
         private Response<EmpInfoFinger> GetLogs(FingerGetAllInput input)
         {
+
             var response = new Response<EmpInfoFinger>();
             var axCZKEM1 = new zkemkeeper.CZKEM();
+            
+           
+            
             bool bIsConnected = false;
             int iMachineNumber = 1;
             try
             {
+              
                 int idwErrorCode = 0;
                 bIsConnected = axCZKEM1.Connect_Net(input.ip, Convert.ToInt32(input.port));
                 if (bIsConnected == true)
                 {
+
                     iMachineNumber = 1;//In fact,when you are using the tcp/ip communication,this parameter will be ignored,that is any integer will all right.Here we use 1.
                     axCZKEM1.RegEvent(iMachineNumber, 65535);//Here you can register the realtime events that you want to be triggered(the parameters 65535 means registering all)
                 }
@@ -247,7 +253,7 @@ namespace HR_System_Backend.Repository.Repository
                 int idwSecond = 0;
                 int idwWorkcode = 0;
                 int idwErrorCode2 = 0;
-
+                var emps = _context.Employees;
                 axCZKEM1.EnableDevice(iMachineNumber, false);//disable the device
                 List<EmpInfoFinger> userList = new List<EmpInfoFinger>();
                 if (axCZKEM1.ReadGeneralLogData(iMachineNumber))//read all the attendance records to the memory
@@ -256,13 +262,15 @@ namespace HR_System_Backend.Repository.Repository
                     while (axCZKEM1.SSR_GetGeneralLogData(iMachineNumber, out sdwEnrollNumber, out idwVerifyMode,
                            out idwInOutMode, out idwYear, out idwMonth, out idwDay, out idwHour, out idwMinute, out idwSecond, ref idwWorkcode))//get records from the memory
                     {
+                        var name = emps.Where(s => s.Code == Int32.Parse(sdwEnrollNumber)).FirstOrDefault().Name;
                         userList.Add(new EmpInfoFinger
                         {
                             idwEnrollNumber = Int32.Parse(sdwEnrollNumber),
                             idwVerifyMode = idwVerifyMode,
                             idwInOutMode = idwInOutMode,
                             LogDate = new DateTime(idwYear, idwMonth, idwDay),
-                            LogTime = new TimeSpan(idwHour, idwMinute, idwSecond)
+                            LogTime = new TimeSpan(idwHour, idwMinute, idwSecond),
+                            name=name
                         });
                     }
                     axCZKEM1.EnableDevice(iMachineNumber, true);//enable the device
